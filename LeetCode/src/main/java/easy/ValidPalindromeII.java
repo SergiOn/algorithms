4f5d1d27580d36dd322dbf52a79070752106e9e5
SergiOn <onishhenko@gmail.com>
2020-05-19 15:45:20 +0300

    #leetcode # easy # 680. Valid Palindrome II (variant 2)

--- LeetCode/src/main/java/easy/ValidPalindromeII.java
+++ LeetCode/src/main/java/easy/ValidPalindromeII.java
@@ -26,55 +26,11 @@ public class ValidPalindromeII {
     public boolean validPalindrome(String s) {
         int leftIndex = 0;
         int rightIndex = s.length() - 1;
-        int deletedLeftChars = 0;
-        int deletedRightChars = 0;
-
-        while (leftIndex < rightIndex) {
-            final char leftChar = s.charAt(leftIndex);
-            final char rightChar = s.charAt(rightIndex);
-
-            if (leftChar == rightChar) {
-                leftIndex += 1;
-                rightIndex -= 1;
-            } else if (leftIndex + 1 != rightIndex) {
-                if (leftChar == s.charAt(rightIndex - 1)) {
-                    deletedRightChars += 1;
-                    rightIndex -= 1;
-                } else if (s.charAt(leftIndex + 1) == rightChar) {
-                    deletedLeftChars += 1;
-                    leftIndex += 1;
-                } else {
-                    leftIndex += 1;
-                    rightIndex -= 1;
-                    deletedLeftChars += 1;
-                    deletedRightChars += 1;
-                }
-            } else {
-                leftIndex += 1;
-            }
-        }
-
-        if ((deletedLeftChars + deletedRightChars) > 1) {
-            return false;
-        }
-
-        return true;
-    }
-}
-
-/*
-* public boolean validPalindrome(String s) {
-        int leftIndex = 0;
-        int rightIndex = s.length() - 1;
         int deletedChars = 0;
         return validPalindromeFromIndex(s, leftIndex, rightIndex, deletedChars);
     }
 
     private boolean validPalindromeFromIndex(final String s, int leftIndex, int rightIndex, int deletedChars) {
-        if (deletedChars > 1) {
-            return false;
-        }
-
         while (leftIndex < rightIndex) {
             if (s.charAt(leftIndex) == s.charAt(rightIndex)) {
                 leftIndex += 1;
@@ -83,11 +39,10 @@ public class ValidPalindromeII {
                 return false;
             } else {
                 return validPalindromeFromIndex(s, leftIndex + 1, rightIndex, deletedChars + 1)
-                    || validPalindromeFromIndex(s, leftIndex, rightIndex - 1, deletedChars + 1);
+                        || validPalindromeFromIndex(s, leftIndex, rightIndex - 1, deletedChars + 1);
             }
         }
 
         return true;
     }
-*
-* */
+}
